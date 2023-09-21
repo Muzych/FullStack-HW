d750e8bc8ba26bc4e1bcec6ac529b0491a192f64
Li0329 <haxk1024@gmail.com>
2023-09-21 22:03:48 +0800

    feat: upload second homework - React Practice

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,64 +1,104 @@
+import { useState } from 'react';
 
-const Header = (props) => {
+
+const Banner = ({text}) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1><b>{text}</b></h1>
     </div>
   )
 }
 
-const Part = (props) => {
+const StatisticLine = ({text, value}) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+      <tr>
+      <td><b>{text}: {value}</b></td>
+      </tr>
   )
 }
+const Statistics = ({good, neutral, bad}) => {
 
-const Content = (props) => {
-  return (
+  if (good === 0 && neutral === 0 && bad === 0){
+    return(
+      <div>
+        <h3>
+          <b>No feedback given</b>
+        </h3>
+      </div>
+    )
+  }
+  return(
     <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      <table>
+        <tbody>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good + neutral + bad}/>
+      <StatisticLine text="average" value={(good + neutral + bad)/3}/>
+      <StatisticLine text="positive" value={good / (good + neutral + bad)} />
+      </tbody>
+      </table>
     </div>
   )
 }
 
-const Total = (props) => {
+const Button = ({text, handClick}) => {
+
   return (
-    <>
     <div>
-      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      <button onClick={handClick}>{text}</button>
     </div>
-    </>
   )
+  
 }
+
+
+
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(8).fill(0))
+
+
+  const handleVotes = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
   }
+
+  const maxVotesIndex = votes.indexOf(Math.max(...votes))
+
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Banner text={"give feedback"}/>
+      <Button text={"good"} handClick={()=>setGood(good + 1)}/>
+      <Button text={"neutral"} handClick={()=>setNeutral(neutral + 1)}/>
+      <Button text={"bad"} handClick={()=>setBad(bad+1)}/>
+      <Banner text={"statistics"}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+      {anecdotes[selected]} <br />
+      has {votes[selected]} votes
+      <Button text={"vote"} handClick={handleVotes}/>
+      <Button text={"next anecdote"} handClick={()=>setSelected(Math.floor(Math.random()*7))}/>
+      <Banner text={"Anecdote with most votes"} />
+      {anecdotes[maxVotesIndex]} <br />
+      has {votes[maxVotesIndex]} votes
     </div>
   )
 }
