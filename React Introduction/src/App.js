01de61ff66b8f214c40214aeeb92f2a696c62d83
Li0329 <haxk1024@gmail.com>
2023-11-06 12:27:04 +0800

    feat: upload exercise 1.6-1.11

--- React Introduction/src/App.js
+++ React Introduction/src/App.js
@@ -1,63 +1,91 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
 
-const Content = (props) => {
+const Header = ({text}) => {
   return (
-    <> 
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    <>
+      <h1>{text}</h1>
     </>
   )
 }
 
-const Part = (props) => {
+const Button = ({handClick, text}) => {
   return (
-  <>
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  </>
+    <>
+      <button onClick={handClick}>{text}</button>
+    </>
   )
 }
 
-const Total = (props) => {
+const StatisticLine = ({text, value}) => {
+  
+  if (text === "positive") {
+    return <p>{text}: {value*100}%</p>
+  }
+  else {
+    return (
+      <>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+      </>
+    )
+  }
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good || neutral || bad){
+    return(
+      <div>
+        <StatisticLine text="good" value ={good} />
+        <StatisticLine text="neutral" value ={neutral} />
+        <StatisticLine text="bad" value ={bad} />
+        <StatisticLine text="all" value={good+neutral+bad}/>
+        <StatisticLine text="average" value={(good+neutral-bad)/good+neutral+bad}/>
+        <StatisticLine text="positive" value={good/(good+neutral+bad)}/>
+      </div>
+    )
+  }
+  else {
+    return <p>No feedback given</p>
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  
+
+  const goodClick = () => {
+    setGood(good + 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      <p>Number of exercises {props.parts.reduce((accumulator, part)=>accumulator + part.exercises, 0)}</p>
+      <Header text={"give feedaback"}/>
+      <Button text={"good"} handClick={goodClick}/>
+      <Button text={"neutral"} handClick={neutralClick}/>
+      <Button text={"bad"} handClick={badClick}/>
+      <Header text={"statics"}/>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
-
 
 export default App
